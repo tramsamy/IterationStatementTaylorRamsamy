ac021f7c395c1caaf035f7b704f8c767a7f11121
Taylor Ramsamy <tramsamy@usf.edu>
2020-02-19 16:26:36 -0500

    Add project files.

---
+++ IterationStatementTaylorRamsamy/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace IterationStatementTaylorRamsamy
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please input a number between 2 and 10.");
+			String NumberInput;
+			NumberInput = Console.ReadLine();
+			int Number;
+			Number = Convert.ToInt32(NumberInput);
+			Console.ReadLine();
+
+			if (Number % 2 == 1)
+
+			{
+				int Count;
+				Count = 0;
+				while (Count < Number - 1)
+				{
+					Count++;
+					Console.WriteLine(Count + ". You have entered an odd number");
+					
+				}
+
+			}
+
+			else
+
+			{
+				int Count;
+				Count = 0;
+				while (Count < Number + 1)
+				{
+					Count++;
+					Console.WriteLine(Count + ". You have entered an odd number");
+					
+				}
+			}
+		}
+	}
+}
